0c581111214801d59a6c2c47a527dcbb7e897881
ChrisKw0n <ckwo873@aucklanduni.ac.nz>
2025-08-21 21:42:02 +1200

    fix: make form start under header

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navigation />
-        <main>{children}</main>
+  <main className="pt-16">{children}</main>
         <Footer />
       </body>
     </html>
